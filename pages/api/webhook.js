609c57ece44bac3445f9e21a5453158a30d926bd
Muhammad Fareed <81671003+mfareed1947@users.noreply.github.com>
2024-05-04 10:06:44 +0500

    first commit

---
+++ pages/api/webhook.js
@@ -0,0 +1,45 @@
+import {mongooseConnect} from "@/lib/mongoose";
+const stripe = require('stripe')(process.env.STRIPE_SK);
+import {buffer} from 'micro';
+import {Order} from "@/models/Order";
+
+const endpointSecret = "whsec_da97a8bd5028fd8be0479625a17ff88e368131470d3e4d6c2371e1701bc6bdbe";
+
+export default async function handler(req,res) {
+  await mongooseConnect();
+  const sig = req.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(await buffer(req), sig, endpointSecret);
+  } catch (err) {
+    res.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case 'checkout.session.completed':
+      const data = event.data.object;
+      const orderId = data.metadata.orderId;
+      const paid = data.payment_status === 'paid';
+      if (orderId && paid) {
+        await Order.findByIdAndUpdate(orderId,{
+          paid:true,
+        })
+      }
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  res.status(200).send('ok');
+}
+
+export const config = {
+  api: {bodyParser:false,}
+};
+
+// bright-thrift-cajole-lean
+// acct_1Lj5ADIUXXMmgk2a
